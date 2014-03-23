abe59db30c75344bf089dbcfbb5b61b2b04d9a9c
tvkanters <tvkanters@gmail.com>
2014-03-23 23:02:41 +0100

    Added first GUI draft.

--- Dopestreamer/src/com/dopelives/dopestreamer/Initialiser.java
+++ Dopestreamer/src/com/dopelives/dopestreamer/Initialiser.java
@@ -1,9 +1,32 @@
 package com.dopelives.dopestreamer;
 
-public class Initialiser {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Initialiser extends Application {
+
+    /** The absolute path to the folder containing the GUI resources */
+    public static final String RESOURCE_FOLDER = "/com/dopelives/dopestreamer/res/";
+    /** The absolute path to the folder containing the GUI image resources */
+    public static final String IMAGE_FOLDER = RESOURCE_FOLDER + "images/";
+
+    /** The window title */
+    private static final String TITLE = "Dopestreamer 420";
 
     public static void main(final String[] args) {
+        Application.launch(Initialiser.class, args);
+    }
 
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final Parent root = FXMLLoader.load(getClass().getResource(RESOURCE_FOLDER + "main_window.fxml"));
+
+        stage.setTitle(TITLE);
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
     }
 
 }
---
+++ Dopestreamer/src/com/dopelives/dopestreamer/MainWindowController.java
@@ -0,0 +1,91 @@
+package com.dopelives.dopestreamer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Callback;
+
+public class MainWindowController implements Initializable {
+
+    @FXML
+    private RadioButton channelCustom;
+    @FXML
+    private TextField channelCustomInput;
+    @FXML
+    private ComboBox<StreamService> streamServiceSelection;
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        // Select the custom channel radio button upon focusing the text field next to it
+        channelCustomInput.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(final ObservableValue<? extends Boolean> observable, final Boolean oldValue,
+                    final Boolean newValue) {
+                if (newValue) {
+                    channelCustom.setSelected(true);
+                }
+            }
+        });
+
+        // Add stream services to the combo box
+        final ObservableList<StreamService> streamServices = streamServiceSelection.getItems();
+        streamServices.add(new StreamService("Hitbox", "hitbox.png"));
+        streamServices.add(new StreamService("Twitch", "twitch.png"));
+
+        // Make the stream services look nice within the combo box
+        streamServiceSelection.setCellFactory(new Callback<ListView<StreamService>, ListCell<StreamService>>() {
+            @Override
+            public ListCell<StreamService> call(final ListView<StreamService> param) {
+                return new ListCell<StreamService>() {
+                    @Override
+                    public void updateItem(final StreamService item, final boolean empty) {
+                        super.updateItem(item, empty);
+
+                        if (item == null) {
+                            return;
+                        }
+
+                        setText(item.label);
+                        setGraphic(new ImageView(new Image(Initialiser.IMAGE_FOLDER + item.icon)));
+                    }
+                };
+            }
+        });
+
+        // Select the first stream service by default
+        streamServiceSelection.setValue(streamServices.get(0));
+    }
+
+    @FXML
+    protected void onLiveClicked(final ActionEvent event) {
+        // Start stream
+    }
+
+    /**
+     * The data class for a stream service that can be selected in the combo box.
+     */
+    private class StreamService {
+        /** The label to show in the combo box */
+        public final String label;
+        /** The icon to show in the combo box */
+        public final String icon;
+
+        public StreamService(final String label, final String icon) {
+            this.label = label;
+            this.icon = icon;
+        }
+    }
+}
