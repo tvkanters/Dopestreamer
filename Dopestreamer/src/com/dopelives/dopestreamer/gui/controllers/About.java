6377c44b9f6fe54a9b15d552983ba3a7949c13d9
tvkanters <tvkanters@gmail.com>
2014-04-02 13:19:09 +0200

    Added links to about page.

--- Dopestreamer/src/com/dopelives/dopestreamer/gui/controllers/About.java
+++ Dopestreamer/src/com/dopelives/dopestreamer/gui/controllers/About.java
@@ -1,5 +1,9 @@
 package com.dopelives.dopestreamer.gui.controllers;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,9 +20,46 @@ public class About implements Initializable {
 
     @Override
     public synchronized void initialize(final URL location, final ResourceBundle resources) {
-
         version.setText("Version " + Environment.VERSION);
+    }
 
+    @FXML
+    public void onEzidClicked() {
+        openUrl("http://tvkdevelopment.com/");
+    }
+
+    @FXML
+    public void onXphomeClicked() {
+        openUrl("http://vacker.me/");
+    }
+
+    @FXML
+    public void onDopelivesClicked() {
+        openUrl("http://dopelives.com/");
+    }
+
+    @FXML
+    public void onLivestreamerClicked() {
+        openUrl("https://github.com/chrippa/livestreamer");
+    }
+
+    @FXML
+    public void onRtmpDumpClicked() {
+        openUrl("http://rtmpdump.mplayerhq.hu/");
+    }
+
+    /**
+     * Opens a given URL in the default browser.
+     * 
+     * @param url
+     *            The URL to open
+     */
+    private void openUrl(final String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (final IOException | URISyntaxException ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
