e9fe8bcd156bb56cd9398cc8f0bd1e878e2b9734
tvkanters <tvkanters@gmail.com>
2015-02-19 23:16:21 +0100

    Improved scalability and readability of output logger

--- Dopestreamer/src/com/dopelives/dopestreamer/util/OutputSpy.java
+++ Dopestreamer/src/com/dopelives/dopestreamer/util/OutputSpy.java
@@ -3,6 +3,9 @@ package com.dopelives.dopestreamer.util;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,11 +14,18 @@ import java.util.List;
  */
 public class OutputSpy extends OutputStream {
 
+    /** The maximum amount of lines to store */
+    private static final int NUM_LINES = 25;
+    /** The formatter for time prefixes per line */
+    private static final SimpleDateFormat sTimeFormatter = new SimpleDateFormat("[HH:mm:ss] ");
+
     /** The original output stream to redirect output to */
     private final OutputStream mOutputStream;
 
     /** The output that has been seen so far as a list of bytes */
-    private final List<Integer> mSavedOutput = new LinkedList<>();
+    private final List<Integer> mTempOutput = new LinkedList<>();
+    /** The last seen lines with up to NUM_LINES entries */
+    private final List<Integer[]> mSavedOutput = new ArrayList<>(NUM_LINES);
 
     /**
      * Remembers all output for logging purposes. Will still send the output to the original stream.
@@ -32,8 +42,33 @@ public class OutputSpy extends OutputStream {
      */
     @Override
     public void write(final int b) throws IOException {
-        mSavedOutput.add(b);
+        // Print to console
         mOutputStream.write(b);
+
+        // Split sentences on newlines
+        if (b == 10 || b == 13) {
+            if (mTempOutput.size() > 0) {
+                // Make sure the output doesn't exceed the size limit
+                if (mSavedOutput.size() == NUM_LINES) {
+                    mSavedOutput.remove(0);
+                }
+
+                // Add time prefix
+                final int[] time = sTimeFormatter.format(Calendar.getInstance().getTime()).chars().toArray();
+                for (int i = 0; i < time.length; ++i) {
+                    mTempOutput.add(i, time[i]);
+                }
+
+                // Store line
+                mSavedOutput.add(mTempOutput.toArray(new Integer[mTempOutput.size()]));
+
+                // Prepare for the next line
+                mTempOutput.clear();
+            }
+        } else {
+            // Add temp output for processing later
+            mTempOutput.add(b);
+        }
     }
 
     /**
@@ -45,23 +80,16 @@ public class OutputSpy extends OutputStream {
     public void writeToFile(final String filename) {
         try {
             final FileOutputStream out = new FileOutputStream(filename);
-            out.write(getSavedOutputAsBytes());
+            for (final Integer[] line : mSavedOutput) {
+                for (final Integer character : line) {
+                    out.write(character);
+                }
+                out.write('\n');
+            }
             out.close();
 
         } catch (final IOException ex) {
             ex.printStackTrace();
         }
-    }
-
-    /**
-     * @return The saved output converted to a byte array
-     */
-    private byte[] getSavedOutputAsBytes() {
-        final byte[] bytes = new byte[mSavedOutput.size()];
-        int i = 0;
-        for (final Integer byteInt : mSavedOutput) {
-            bytes[i++] = byteInt.byteValue();
-        }
-        return bytes;
     }
 }
